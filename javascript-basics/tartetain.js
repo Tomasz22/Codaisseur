69a5ff358bc88548e52e0c5b24c411d760e4cd6b
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-09 16:55:58 +0100

    did follw the calculating task

---
+++ javascript-basics/tartetain.js
@@ -0,0 +1,20 @@
+
+
+// total weight of 9 portions
+console.log(9 * (300 + 900 + 100 + 85 + 60)) // 13005
+
+//fat per portion
+
+/*
+
+ in 6 portions I have thus () /6
+*(
+ butter 100% fat  and I got there 85g in 6 portions
+*
+ pastry in 6 portions I have 300g and in 100g I have 26.4g as fat
+ thus 26.4  * 3
+ )
+*/ 
+
+
+console.log((85 +26.4 *3)/6)
