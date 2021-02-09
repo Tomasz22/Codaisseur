be877bdcf182741d2004a8f178b6dd3211fdd280
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-09 13:28:10 +0100

    I have  tested via  printing all data types

---
+++ javascript-basics/tasting.js
@@ -0,0 +1,8 @@
+//tasting.js 
+console.log('We are going to taste all the datatypes!')
+console.log(42, 23 , 2)
+console.log(false)
+console.log(undefined)
+console.log(null)
+console.log({name:"Tomasz", age: null, lottery: [22, 4, 85 , 7]})
+console.log([1, 2, 3, 5, 8]);
