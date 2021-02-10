bd08e3002e4c97e2f14f8638a427b4228962e34f
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-10 11:36:52 +0100

    tested ny solution in standard if statements and it worked

--- javascript-basics/accessDenied.js
+++ javascript-basics/accessDenied.js
@@ -7,7 +7,6 @@ const passwordCorrect = password === input
 const finalLoginAttempt = loginAttemptsFailed + 1 >= maxLoginAttempts
 
 const accountLocked = !passwordCorrect && finalLoginAttempt
-
 // just locking someone out of their account isn't very nice ...
 // We want to display some feedback:
 
@@ -17,3 +16,39 @@ const accountLocked = !passwordCorrect && finalLoginAttempt
 // 'Wrong! You can try again x more time(s)'
 
 
+
+const mySolution = () => {
+    if(passwordCorrect){
+        return 'Welcome'
+    }else if(finalLoginAttempt && !passwordCorrect){
+        return `Wrong! You can try again ${maxLoginAttempts - loginAttemptsFailed} more time(s)`
+    }else if(!passwordCorrect && maxLoginAttempts<=maxLoginAttempts){
+        return 'You are trying to hack us! ACCESS DENIED!'
+    }else{
+        'Please log in'
+    }
+}
+
+
+// const display = (passwordCorrect
+//     ?'Welcome'
+//     :finalLoginAttempt && !passwordCorrect
+//     ?`Wrong! You can try again ${maxLoginAttempts - loginAttemptsFailed} more time(s)`)
+//     :!passwordCorrect && maxLoginAttempts<=maxLoginAttempts
+//     ?'You are trying to hack us! ACCESS DENIED!'
+//     :
+//     )
+
+// console.log(display)
+
+console.log(mySolution());
+
+
+const message = (passwordCorrect
+  ? 'Welcome'
+  : accountLocked
+  ? 'You are trying to hack us! ACCESS DENIED!'
+  : `Wrong! You can try again ${maxLoginAttempts - (loginAttemptsFailed + 1)} more time(s)`
+)
+
+console.log(message)
