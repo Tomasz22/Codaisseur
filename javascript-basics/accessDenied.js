d6e42161cf75e80c8a71e6a47f0e1bebf084eef5
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-10 09:18:48 +0100

    tested the outcome of the code

--- javascript-basics/accessDenied.js
+++ javascript-basics/accessDenied.js
@@ -1,30 +1,32 @@
-// const password = 'supersecretpassword'
-// const input = 'iamhackingyou'
-// const loginAttemptsFailed = 2
-// const maxLoginAttempts = 3
+ const password = 'supersecretpassword'
+ const input = 'iamhackingyou'
+ const loginAttemptsFailed = 2
+ const maxLoginAttempts = 3
 
-// const passwordCorrect = password === input
-// const finalLoginAttempt = loginAttemptsFailed + 1 >= maxLoginAttempts
+ const passwordCorrect = password === input
+ const finalLoginAttempt = loginAttemptsFailed + 1 >= maxLoginAttempts
 
-// const accountLocked = !passwordCorrect && finalLoginAttempt
+ const accountLocked = !passwordCorrect && finalLoginAttempt
 
-// // just locking someone out of their account isn't very nice ...
-// // We want to display some feedback:
+ // just locking someone out of their account isn't very nice ...
+ // We want to display some feedback:
 
 // // If the password is correct -> 'Welcome'
-// // If the account was locked -> 'You are trying to hack us! ACCESS DENIED!'
-// // If the password was wrong but you can try again -> 
-// // 'Wrong! You can try again x more time(s)'
+ // If the account was locked -> 'You are trying to hack us! ACCESS DENIED!'
+// If the password was wrong but you can try again -> 
+// 'Wrong! You can try again x more time(s)'
 
 
-// const feedback = (
-//     passwordCorrect
-//     ?'Welcome'
-//     : !passwordCorrect && finalLoginAttempt
-//     ?`Wrong! You can try again ${maxLoginAttempts - loginAttemptsFailed} more time(s)`
-//     :'You are trying to hack us! ACCESS DENIED!'
-// );
 
-// console.log(feedback);
+const feedback = (
+     passwordCorrect
+     ?'Welcome'
+     : !passwordCorrect && finalLoginAttempt
+     ?`Wrong! You can try again ${maxLoginAttempts - loginAttemptsFailed} more time(s)`
+     :!passwordCorrect && !finalLoginAttempt
+     ?'You are trying to hack us! ACCESS DENIED!'
+ )
+
+ console.log(feedback);
 
 console.log('just to test it');
