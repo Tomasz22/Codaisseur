be1c2b99dcf856dc885ac1b69cb55268de9b2541
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-10 17:08:34 +0100

    did if-statement exercises

---
+++ javascript-basics/if_statement.js
@@ -0,0 +1,11 @@
+let age = 8 
+if (age >= 18){
+    console.log('adult')
+    console.log('We can add statements inside this block!')
+}
+else if (age < 18 && age > 12){
+    console.log('teenager')
+}else{
+    console.log('child')
+}
+
