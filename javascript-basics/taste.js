197b142b087ba3c0985982d4121aed57b31e1f56
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-10 17:26:12 +0100

    did switch exercise

--- javascript-basics/taste.js
+++ javascript-basics/taste.js
@@ -1,14 +1,30 @@
-const command = '';
-switch (command) {
-  case 'prepare starter':
-    console.log('I am preparing a starter!');
-    break;
-  case 'prepare main':
-    console.log('I am preparing a main!');
-    break;
-  case 'prepare dessert':
-    console.log('I am preparing dessert!');
-    break;
-  default:
-    console.log('I am cleaning!');
+// const command = 'prepare main';
+// switch (command) {
+//   case 'prepare starter':
+    // console.log('I am preparing a starter!');
+    // break;
+//   case 'prepare main':
+    // console.log('I am preparing a main!');
+    // break;
+//   case 'prepare dessert':
+    // console.log('I am preparing dessert!');
+    // break;
+//   default:
+    // console.log('I am cleaning!');
+// }
+
+
+const taste = 'good'
+
+switch (taste){
+    case 'good':
+        console.log('I like this!It is very tasty!')
+        break;
+    case 'ok':
+        console.log('It is tasty.')
+        break;
+    //case 'terrible':
+        default:
+        console.log('Ughhh this food is malevolently bad!!!')
+        break;
 }
