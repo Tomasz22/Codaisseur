843a0e1cacff7f69e05eb5190bff16e4fd4c09f6
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-10 17:14:37 +0100

    tested the syntax in the reader

---
+++ javascript-basics/taste.js
@@ -0,0 +1,14 @@
+const command = '';
+switch (command) {
+  case 'prepare starter':
+    console.log('I am preparing a starter!');
+    break;
+  case 'prepare main':
+    console.log('I am preparing a main!');
+    break;
+  case 'prepare dessert':
+    console.log('I am preparing dessert!');
+    break;
+  default:
+    console.log('I am cleaning!');
+}
