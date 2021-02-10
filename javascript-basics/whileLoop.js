60d911322a4b9199898d18673f72a78b353891f8
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-10 18:09:13 +0100

    did while loop exercise

---
+++ javascript-basics/whileLoop.js
@@ -0,0 +1,15 @@
+let number = 1
+while (number < 10) {
+    console.log(number)
+     number++;
+}
+
+
+let letters = ["a", "b", "c"]
+let index = 0
+
+while (index < letters.length) {
+    console.log(letters[index])
+    index++;
+    /* reassign index to index + 1 */
+}
